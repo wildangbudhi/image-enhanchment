ef1d53f911d4b67d082784ee85d09086db8f4363
ersadishla <ersadishlahuddin@gmail.com>
2021-03-24 07:38:44 +0700

    added comparison

--- main.py
+++ main.py
@@ -24,16 +24,16 @@ class ImageProcessor():
             elif( code == ord('n') ):
                 self.to_negatif()
             elif( code == ord('h') ):
+                #use xray_penuomia
                 self.hist_eq()
             elif( code == ord('l') ):
+                #use logaritmik
                 self.log_transform()
-            # elif( code == ord('p') ):
-            #     self.power_law()
             elif( code == ord('m') ):
                 #use brain image
                 self.median_filtering()
             elif( code == ord('c') ):
-                #use  xray_penuomia
+                #use xray_penuomia
                 self.contrast_streching()
 
     def show( self ):
@@ -43,28 +43,28 @@ class ImageProcessor():
         self.processed_image = self.original_image
 
     def to_negatif( self ):
-        self.processed_image = 255.0 - self.processed_image
+        self.processed_image = 255 - self.processed_image
+        self.processed_image = cv.hconcat( [ self.original_image, self.processed_image ] )
 
     def hist_eq( self ):
         self.processed_image = cv.equalizeHist( self.processed_image )
+        self.processed_image = cv.hconcat( [ self.original_image, self.processed_image ] )
 
     def log_transform( self ):
         self.processed_image = np.array( 100.0 * np.log10( 1.0 + self.processed_image ), dtype=np.uint8 )
-
-    # def power_law( self ):
-    #     first_gamma = np.array( 255.0 * ( self.processed_image / 255.0 ) ** 0.3, dtype=np.uint8 )
-    #     second_gamma = np.array( 255.0 * ( self.processed_image / 255.0 ) ** 0.6, dtype=np.uint8 )
-    #     self.processed_image = cv.hconcat( [ first_gamma, second_gamma ] )
+        self.processed_image = cv.hconcat( [ self.original_image, self.processed_image ] )
 
     def median_filtering( self ):
-        median = cv.medianBlur(self.processed_image, 5)
-        self.processed_image = np.concatenate((self.processed_image, median), axis=1) #membandingkan
+        self.processed_image = cv.medianBlur(self.processed_image, 5)
+        self.processed_image = cv.hconcat( [ self.original_image, self.processed_image ] )
+        # self.processed_image = np.concatenate((self.processed_image, self.processed_image), axis=1) #membandingkan
 
     def contrast_streching( self ):
-        norm_img = cv.normalize(self.processed_image, None, alpha=0, beta=1.2, norm_type=cv.NORM_MINMAX, dtype=cv.CV_32F)
-        norm_img = np.clip(norm_img, 0, 1)
-        norm_img = (255*norm_img).astype(np.uint8)
-        self.processed_image = np.concatenate((self.processed_image, norm_img), axis=1) #membandingkan
+        self.processed_image = cv.normalize(self.processed_image, None, alpha=0, beta=1.2, norm_type=cv.NORM_MINMAX, dtype=cv.CV_32F)
+        self.processed_image = np.clip(self.processed_image, 0, 1)
+        self.processed_image = (255*self.processed_image).astype(np.uint8)
+        self.processed_image = cv.hconcat( [ self.original_image, self.processed_image ] )
+        # self.processed_image = np.concatenate((self.processed_image, self.processed_image), axis=1) #membandingkan
 
 
 def main():
